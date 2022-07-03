234ce01ce36d2ccae2c59d5833a719b8ae6d4679
Yavuz İslam <100232592+yavuzislm@users.noreply.github.com>
2022-07-03 16:09:05 +0300

    Add files via upload

---
+++ libft/ft_strmapi.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yislam <yislam@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/28 13:28:43 by yislam            #+#    #+#             */
+/*   Updated: 2022/06/28 13:54:15 by yislam           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int	i;
+	char			*str;
+
+	if (!s || !f)
+		return (NULL);
+	i = 0;
+	str = (char *) ft_calloc(ft_strlen(s) + 1, sizeof(char));
+	if (!str)
+		return (NULL);
+	while (s[i])
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	return (str);
+}
+
+/*
+char	ft_change(unsigned int i, char b)
+{
+	b += i;
+	return (b);
+}
+
+int	main()
+{
+	char	*a;
+
+	a = "aaaaaa";
+	printf("%s", ft_strmapi(a, &ft_change));
+}
+*/
---
+++ libft/ft_strnstr.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yislam <yislam@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/22 13:52:02 by yislam            #+#    #+#             */
+/*   Updated: 2022/06/30 14:32:00 by yislam           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	h;
+	size_t	n;
+
+	h = 0;
+	if (needle[0] == '\0')
+		return ((char *)haystack);
+	while (haystack[h] != '\0')
+	{
+		n = 0;
+		while (haystack[h + n] == needle[n] && (h + n) < len)
+		{
+			if (haystack[h + n] == '\0' && needle[n] == '\0')
+				return ((char *)&haystack[h]);
+			n++;
+		}
+		if (needle[n] == '\0')
+			return ((char *)haystack + h);
+		h++;
+	}
+	return (0);
+}
+/*
+int main(void)
+{
+	char *yavuz = "yavuz talha islam";
+	char *talha = "talha";
+
+	printf("%s",ft_strnstr(yavuz,talha,14));
+}
+*/
+
+/*
+özel durum 
+if (!needle || !needle[0])
+		return ((char *)haystack);
+*/
