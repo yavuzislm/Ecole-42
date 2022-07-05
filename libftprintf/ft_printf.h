7a153161f204cf591a791f6135048de11af63e3f
Yavuz İslam <100232592+yavuzislm@users.noreply.github.com>
2022-07-06 01:27:48 +0300

    Add files via upload

---
+++ libftprintf/ft_printf.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yislam <yislam@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/04 14:24:39 by yislam            #+#    #+#             */
+/*   Updated: 2022/07/05 22:27:21 by yislam           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include "libft/libft.h"
+
+int		ft_printf(const	char *str, ...);
+int		ft_putchar(char x);
+int		ft_putstring(char *str);
+int		ft_uitoa(unsigned int n);
+int		ft_decimal(unsigned int nbr);
+int		ft_dtoh(unsigned long x);
+#endif
---
+++ libftprintf/libft/ft_strmapi.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yislam <yislam@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/28 13:28:43 by yislam            #+#    #+#             */
+/*   Updated: 2022/07/04 11:28:22 by yislam           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int	i;
+	char			*str;
+
+	if (!s || !f)
+		return (NULL);
+	i = 0;
+	str = (char *) ft_calloc(ft_strlen(s) + 1, sizeof(char));
+	if (!str)
+		return (NULL);
+	while (s[i])
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	return (str);
+}
